74b973d44408d63b3e2e5f5b941f6a580485034a
serikbatyrkhanov <Serik3571891>
2024-06-11 14:57:11 -0400

    formatted code

--- Modal/starter/script.js
+++ Modal/starter/script.js
@@ -7,25 +7,24 @@ const btnsOpenModal = document.querySelectorAll('.show-modal');
 
 console.log(btnsOpenModal);
 
-const openModal = function(){
-    modal.classList.remove('hidden');
-    overlay.classList.remove('hidden');
-}
-const closeModal = function(){
-    modal.classList.add('hidden');
-    overlay.classList.add('hidden');
-}
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
 
-
-for(let i = 0; i < btnsOpenModal.length; i++){
-    btnsOpenModal[i].addEventListener('click', openModal);
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener('click', openModal);
 }
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
 
-document.addEventListener('keydown', function(e){
-    if(e.key === 'Escape' && !modal.classList.contains('hidden')){
-        closeModal();
-    }
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
 });
